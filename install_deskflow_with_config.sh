a147d8810edad9927cb44e61d120d4c0318b3740
Leo <107443931+lolgufdHD@users.noreply.github.com>
2025-06-05 22:51:02 +0200

    Update install_deskflow_with_config.sh

--- install_deskflow_with_config.sh
+++ install_deskflow_with_config.sh
@@ -36,7 +36,7 @@ echo "⬇️ Downloading Deskflow config..."
 GITHUB_USER="lolgufdHD"  # <-- Change this
 CONFIG_URL="https://raw.githubusercontent.com/$GITHUB_USER/linux-files/main/deskflow_config.conf"
 DEST_DIR="$HOME/.config/deskflow"
-DEST_FILE="$DEST_DIR/config.yaml"
+DEST_FILE="$DEST_DIR/deskflow-server.conf"
 
 mkdir -p "$DEST_DIR"
 curl -fsSL "$CONFIG_URL" -o "$DEST_FILE"
