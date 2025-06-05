4f3e64f8f88d5a57038015fa290745f3894a6203
Leo <107443931+lolgufdHD@users.noreply.github.com>
2025-06-05 22:56:25 +0200

    Update install_deskflow_with_config.sh

--- install_deskflow_with_config.sh
+++ install_deskflow_with_config.sh
@@ -8,7 +8,7 @@ echo "Installing Deskflow..."
 if ! command -v deskflow >/dev/null 2>&1; then
     distro=$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '"')
 
-    case "vanilla" in
+    case "$distro" in
         vanilla|ubuntu|debian)
             sudo apt update
             sudo apt install -y deskflow
